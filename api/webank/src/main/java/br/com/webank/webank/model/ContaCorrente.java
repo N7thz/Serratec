e29dad4b05c9a331f6c8484e73f5dc1ce5f5a366
Nathan Ferreira <nathanferreiradev@gmail.com>
2023-10-09 22:02:08 -0300

    adicionei exercicios

---
+++ api/webank/src/main/java/br/com/webank/webank/model/ContaCorrente.java
@@ -0,0 +1,56 @@
+package br.com.webank.webank.model;
+
+import java.util.Date;
+
+public class ContaCorrente {
+
+    private long id;
+    private String agencia;
+    private String numero;
+    private double saldo;
+    private Date dataCadastro;
+
+    
+
+    public ContaCorrente(long id, String agencia, String numero, double saldo, Date dataCadastro) {
+        this.id = id;
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+        this.dataCadastro = new Date();
+    }
+
+    //#region Getter's and Setter's
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getAgencia() {
+            return agencia;
+        }
+
+        public void setAgencia(String agencia) {
+            this.agencia = agencia;
+        }
+
+        public String getNumero() {
+            return numero;
+        }
+
+        public void setNumero(String numero) {
+            this.numero = numero;
+        }
+
+        public double getSaldo() {
+            return saldo;
+        }
+
+        public Date getDataCadastro() {
+            return dataCadastro;
+        }    
+    //#endregion
+}
