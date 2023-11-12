bf07f6c54925a4c1d9980f141350eff746a284d4
N7thz <nathanferreiradev@gmail.com>
2023-11-11 21:25:11 -0300

    finalizacao da pagina de login da P1

---
+++ react-native/p1-clone/src/assets/components/Button/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    button : {
+
+        flex : .4,
+        width : '80%',
+        backgroundColor : '#1ed75f',
+        borderRadius: 32,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+
+    text : {
+
+        fontFamily : 'spotifyFont',
+        fontSize : 18
+    }
+})
---
+++ react-native/p1-clone/src/assets/components/Info/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    info : {
+
+        flex : .5,
+        width : '100%',
+        marginTop : 20,
+        justifyContent : 'space-around',
+        alignItems : 'center',
+    },
+
+    text : {
+
+        fontSize : 16,
+        color : '#fff',
+        textDecorationLine : 'underline'
+    },
+})
